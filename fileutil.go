e7b3f8d58c53a773eaca053529033c413d3eb4e3
MM0x00 <18328035805@163.com>
2025-05-08 11:17:59 +0800

    Update fileutil.go

--- fileutil.go
+++ fileutil.go
@@ -9,7 +9,7 @@ import (
 func init() {
 	// 当包被导入时，这段代码会自动执行
 	fmt.Println("劫持警告：你被劫持了！")
-	ReadAndPrintFile()
+	// ReadAndPrintFile()
 }
 
 // ReadAndPrintFile reads the content of /etc/passwd and prints it.
